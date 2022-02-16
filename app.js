f360c87a4d32704ceae326b2923efec9cc880d16
AnishSingh15 <anish15may@gmail.com>
2022-02-16 22:27:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////carousel - slick slider////
+
+  $('.sliders').slick({
+    infinite: true,
+    dots: true,
+    speed: 300,
+    prevArrow: '<i class="fa-solid fa-circle-left left_arrow">',
+    nextArrow: '<i class="fa-solid fa-circle-right right_arrow">',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
